50883090ded36a3b8faa4ad3e48d8f096382ecec
zheka <zvenglinski@gmail.com>
2022-03-01 23:02:00 -0500

    changes in about, base and index

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,22 +5,18 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"Home" in response.data
 
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"About Page" in response.data
+    assert b"About" in response.data
 
 def test_request_page1(client):
     """This makes the index page"""
